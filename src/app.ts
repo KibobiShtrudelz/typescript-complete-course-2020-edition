6b4e6df28b23c96451dba6b5048d4555850af129
Петър Колев <petar.georgiev.kolev@gmail.com>
2019-11-28 17:08:59 +0200

    94. Creating a Generic Function ( 05m 46s )

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
 // tsc --target es6 app.ts --watch
+
+// const names: Array<string> = ["Peco", "Kleco"]; // string[]
+// names[0].split(" ");
+
+// const promise: Promise<string | number> = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("This is done!");
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   if (typeof data === "string") {
+//     console.log(data.split(" "));
+//   } else {
+//     console.log(data)
+//   }
+// });
+
+// Custom Generic Types
+
+// this way we can notate to specific prop in the objects because
+// we declare in <> that this two objects are intercepting and 
+// therefor TS knows what props are going to be included. This way
+// describe to TS what types we are going to use
+function merge<T, U>(objA: T, objB: U) {
+  return { ...objA, ...objB };
+}
+
+const mergeObj = merge({ name: "Pecimir" }, { age: 30 });
+mergeObj.name
